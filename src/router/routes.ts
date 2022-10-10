834be495cb1371c51579c7a2b911ad91f56c00a5
Daniel Kogut <daniel.kogut@sidearmsports.com>
2022-10-09 19:36:07 -0500

    refector route files

---
+++ src/router/routes.ts
@@ -0,0 +1,74 @@
+export default [
+  {
+    path: '',
+    component: () => import('@/layouts/AuthLayout.vue'),
+    children: [
+      { path: "", redirect: "signin" },
+      {
+        path: "signin",
+        name: "SignIn",
+        component: () =>
+          import(/* webpackChunkName: "auth" */ "@/pages/SignInPage.vue"),
+      },
+      {
+        path: "signup",
+        name: "SignUp",
+        component: () =>
+          import(/* webpackChunkName: "auth" */ "@/pages/SignUpPage.vue"),
+      },
+      {
+        path: "forgotpassword",
+        name: "ForgotPassword",
+        component: () =>
+          import(
+            /* webpackChunkName: "auth" */ "@/pages/ForgotPasswordPage.vue"
+          ),
+      },
+    ],
+    
+  },
+  {
+    path: "",
+    component: () =>
+      import(/* webpackChunkName: "main" */ "@/layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: () =>
+          import(/* webpackChunkName: "main" */ "@/pages/MainPage.vue"),
+      },
+      {
+        path: "classification",
+        name: "Classification",
+        component: () =>
+          import(/* webpackChunkName: "main" */ "@/pages/ClassificationPage.vue"),
+      },
+      {
+        path: "hunch",
+        name: "Hunch",
+        component: () =>
+          import(/* webpackChunkName: "main" */ "@/pages/HunchPage.vue"),
+      },
+      {
+        path: "invite",
+        name: "Invite",
+        component: () =>
+          import(/* webpackChunkName: "main" */ "@/pages/MainPage.vue"),
+      },
+      {
+        path: "moderation",
+        name: "Moderation",
+        component: () =>
+          import(/* webpackChunkName: "main" */ "@/pages/MainPage.vue"),
+      },
+      {
+        path: "edit",
+        name: "Edit",
+        component: () =>
+          import(/* webpackChunkName: "main" */ "@/pages/MainPage.vue"),
+      },
+    ],
+  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import("@/layouts/404Layout.vue") },
+];
